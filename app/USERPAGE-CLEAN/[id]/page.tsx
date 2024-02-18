515d137d0cd6ceeb5e95c62f9667fa7a83ec3f58
Mark Halstead <mark.d.halstead@gmail.com>
2024-02-17 23:03:06 -0700

    Add not-found route for dashboard

--- app/USERPAGE-CLEAN/[id]/page.tsx
+++ app/USERPAGE-CLEAN/[id]/page.tsx
@@ -4,7 +4,7 @@ import getUser from "@/data-access/users/get-user-----";
 import type { UserDto } from "@/use-cases/user/types";
 import { unstable_noStore } from "next/cache";
 import { sessionAuth } from "@/lib/sessionAuth";
-
+import { notFound } from "next/navigation";
 type ParamsType = {
   id: string;
 };
@@ -17,9 +17,15 @@ const UserPage = async ({ params }: { params: ParamsType }) => {
   // if (!session) {
   //   redirect(`/api/auth/signin?callbackUrl=/USERPAGE-CLEAN/${id}`);
   // }
-  const user: UserDto = await getUser(params.id);
-
-  console.log("user received,", user);
+  // console.log("session", session);
+  const sessionUser = session?.user.id;
+  let user: UserDto;
+  if (sessionUser == id) {
+    user = await getUser(id);
+  } else {
+    console.log("user not found");
+    return notFound();
+  }
   // const userIdString = user._id;
   // add params
   // const userId = params.userData.id;
