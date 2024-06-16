61cfeada0ea8f993abe8478bb88a7839e30f4c2d
MarkLovesCoding <mark.d.halstead@gmail.com>
2024-06-15 23:48:14 -0600

    Create delete-task.use-case.ts

---
+++ use-cases/task/delete-task.use-case.ts
@@ -0,0 +1,32 @@
+import { taskToDto } from "@/use-cases/task/utils";
+import { TaskEntity, TaskEntityValidationError } from "@/entities/Task";
+import { AuthenticationError, ValidationError } from "../utils";
+
+import type { UpdateTask, GetTask } from "@/use-cases/task/types";
+import type { GetUserSession } from "@/use-cases/user/types";
+
+export async function deleteTaskUseCase(
+  context: {
+    updateTask: UpdateTask;
+    getTask: GetTask;
+    getUser: GetUserSession;
+  },
+  data: {
+    id: string;
+    project: string;
+    archived: boolean;
+  }
+) {
+  const user = context.getUser();
+  if (!user) throw new AuthenticationError();
+
+  try {
+    const retrievedTask = await context.getTask(data.id);
+    const taskAsEntity = new TaskEntity({ ...retrievedTask });
+    taskAsEntity.updateArchived(data.archived);
+    await context.updateTask(taskToDto(taskAsEntity));
+  } catch (err) {
+    const error = err as TaskEntityValidationError;
+    throw new ValidationError(error.getErrors());
+  }
+}
