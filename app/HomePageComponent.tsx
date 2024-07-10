a01665356c98f301af704c70f5d318e1fd971c66
MarkLovesCoding <mark.d.halstead@gmail.com>
2024-07-10 13:40:50 -0600

    style: homepage updates
    Bold fonts
    Spacing

--- app/HomePageComponent.tsx
+++ app/HomePageComponent.tsx
@@ -80,7 +80,7 @@ const HomePageComponent = () => {
             </Button>
           </Link>
         </div>
-        <div className="   self-center flex flex-col  text-center w-full justify-center items-center h-[80vh] mt-[20vh] min-h-[300px] ">
+        <div className="   self-center flex flex-col  text-center w-full justify-center items-center h-[80vh] mt-[15vh] min-h-[300px] ">
           <h2 className="  bg-gradient-to-r  from-orange-500 via-pink-500 to-purple-500 text-transparent bg-clip-text max-w-[700px] sm:max-w-60%  text-5xl leading-tight tracking-lighter md:text-6xl lg:leading-[1.1] text-orange-500 font-bold p-8 pb-4">
             {" Simplifying Project Management"}
           </h2>
@@ -114,7 +114,7 @@ const HomePageComponent = () => {
             side="left"
           >
             <Image
-              className=" flex top-10 -right-16 md:-right-16 md:top-[50%] w-[200px] sm:w-[250px] lg:w-[280px]"
+              className=" flex top-10 -right-16 md:-right-16 md:top-[50%] w-[270px] sm:w-[250px] lg:w-[280px]"
               src={MoveTask}
               alt="dashboard-image-1"
             />
@@ -133,12 +133,12 @@ const HomePageComponent = () => {
             side="right"
           >
             <Image
-              className=" flex top-10 -right-16 md:-right-16 md:top-[50%] w-[220px] sm:w-[250px] lg:w-[280px]"
+              className=" flex top-10 -right-16 md:-right-16 md:top-[50%] w-[250px] sm:w-[250px] lg:w-[280px]"
               src={ArchiveTaskCard}
               alt="dashboard-image-1"
             />
             <Image
-              className=" flex top-10 -left-16 md:-left-16 md:top-[25%] w-[220px] sm:w-[250px] lg:w-[280px]"
+              className=" flex top-10 -left-16 md:-left-16 md:top-[25%] w-[250px] sm:w-[250px] lg:w-[280px]"
               src={DashboardTeams}
               alt="dashboard-image-1"
             />
@@ -204,8 +204,8 @@ const FeatureSection = forwardRef<HTMLDivElement, FeatureSectionProps>(
     >
       <div className="flex-col self-center items-center flex w-full sm:w-[50%] lg:w-[65%] m-12 md:mr-0  md:m-12">
         <div className="  lg:w-[40vw] mt-12 sm:mt-0 sm:[70vw] w-full px-[10%] sm:px-[0%]">
-          <h2 className="text-2xl text-white"> {title} </h2>
-          <h3 className="text-base text-muted-foreground sm:text-xl p-8 pb-0 pt-4">
+          <h2 className="text-2xl font-bold text-white"> {title} </h2>
+          <h3 className="text-base text-muted-foreground sm:text-xl p-4 pb-0 pt-4">
             {description}
           </h3>
         </div>
